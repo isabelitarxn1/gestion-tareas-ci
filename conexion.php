e31298076d42a7b9fded053e584b52f0112c24b1
isabel <i.c.m.h@hotmail.com>
2026-06-05 13:41:41 -0500

    Implementacion de Docker y comunicacion entre contenedores

--- conexion.php
+++ conexion.php
@@ -1,4 +1,15 @@
+<?php
+
+$host = "db";
+$usuario = "root";
+$password = "root";
+$basedatos = "gestion_tareas_ci";
+
+$conexion = mysqli_connect($host, $usuario, $password, $basedatos);
+
 // Verificar conexión
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
 }
+
+?>
